d1646661683af89e8f250058cfb7a14ae3292c4a
Yeisitaaaa <yeinypao17@gmail.com>
2026-03-13 13:04:42 -0500

    mi primer commit

--- src/view/PanelAdmin.java
+++ src/view/PanelAdmin.java
@@ -32,11 +32,11 @@ public class PanelAdmin extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 846, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 534, Short.MAX_VALUE)
         );
 
         pack();
